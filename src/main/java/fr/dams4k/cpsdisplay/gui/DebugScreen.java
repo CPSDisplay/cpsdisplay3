e95ac0bc2a72f54d6a274c187280b01c191a7b8c
Dams4K <dams4k@users.noreply.github.com>
2023-08-22 20:23:12 +0200

    adding guides

--- src/main/java/fr/dams4k/cpsdisplay/gui/DebugScreen.java
+++ src/main/java/fr/dams4k/cpsdisplay/gui/DebugScreen.java
@@ -9,7 +9,7 @@ public class DebugScreen extends GuiScreen {
         super.drawScreen(mouseX, mouseY, partialTicks);
 
 
-        int c = 0xffff0000;
+        int c = 0x45ff0000;
         int margin = 1;
 
         ScaledResolution scaledresolution = new ScaledResolution(this.mc);
@@ -21,17 +21,44 @@ public class DebugScreen extends GuiScreen {
         this.drawVerticalLine(margin,           margin,             height-margin-1, c);
         this.drawVerticalLine(width-margin-1,   margin,             height-margin-1, c);
         
+
+        // Screen splited
+        this.drawHorizontalLine(margin, width-margin-1, height/2, c);
+        this.drawVerticalLine(width/2, margin, height-margin, c);
+
+        
         // Hotbar
         this.drawHorizontalLine(middle-91-margin,       middle + 91 + margin-1,                   scaledresolution.getScaledHeight()+margin-1, c);
         this.drawHorizontalLine(middle-91-margin,       middle + 91 + margin-1,                   scaledresolution.getScaledHeight()-22-margin, c);
         this.drawVerticalLine(middle-91-margin,         scaledresolution.getScaledHeight()+margin,  scaledresolution.getScaledHeight()-22-margin-1, c);
         this.drawVerticalLine(middle+91+margin-1,       scaledresolution.getScaledHeight()+margin,  scaledresolution.getScaledHeight()-22-margin-1, c);
 
+
         // Scoreboard
-        
+        // Scoreboard scoreboard = mc.theWorld.getScoreboard();
+        // ScoreObjective scoreObjective = null;
+        // EEEEEE
 
         // Bossbar
+        int bossBarWidth = 182;
+        int bossBarHeight = 5;
 
+        int bossBarStartX = scaledresolution.getScaledWidth() / 2 - bossBarWidth / 2;
+        int bossBarStartY = 12;
+        int bossBarEndX = bossBarStartX+bossBarWidth;
+        int bossBarEndY = bossBarStartY+bossBarHeight;
+
+        this.drawHorizontalLine(bossBarStartX-margin, bossBarEndX+margin-1, bossBarStartY-margin, c);
+        this.drawHorizontalLine(bossBarStartX-margin, bossBarEndX+margin-1, bossBarEndY+margin-1, c);
+        this.drawVerticalLine(bossBarStartX-margin, bossBarStartY-margin, bossBarEndY+margin-1, c);
+        this.drawVerticalLine(bossBarEndX+margin-1, bossBarStartY-margin, bossBarEndY+margin-1, c);
+
+
+        // Cross
+        this.drawHorizontalLine(width/2-7-margin, width/2+7+margin, height/2-7-margin, c);
+        this.drawHorizontalLine(width/2-7-margin, width/2+7+margin, height/2+7+margin, c);
+        this.drawVerticalLine(width/2-7-margin, height/2-7-margin, height/2+7+margin, c);
+        this.drawVerticalLine(width/2+7+margin, height/2-7-margin, height/2+7+margin, c);
 
         // System.out.println(width); // get bigger when changing scale
 
